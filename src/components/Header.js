968ac59e99eff7408d4dae9c9d6ff47332606e0f
Madhu062 <madhulikachalla.6@gmail.com>
2023-10-02 11:21:53 -0500

    header changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../App.css';
 import { auth } from '../utils/firebase';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
+    const dispatch = useDispatch();
+
     const navigate = useNavigate();
-const user = useSelector((store) => store.user);
+    const user = useSelector((store) => store.user);
     const handleSignOut = () => {
         signOut(auth)
-        .then(() => {
-            navigate("/");
-        }).catch((error) => {
-            navigate("/error");
-        });
+            .then(() => {
+                navigate("/");
+            }).catch((error) => {
+                navigate("/error");
+            });
     }
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const { uid, email, displayName, photoURL } = user;
+                dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }))
+                navigate("/browse");
+                // ...
+            } else {
+                // User is signed out
+                dispatch(removeUser());
+                navigate("/"); 
+                // ...
+            }
+        });
+    }, [])
     return (
         <div className="headerClass from-black  justify-between ">
             <div>
@@ -24,11 +45,11 @@ const user = useSelector((store) => store.user);
                     src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
                     alt='logo' />
             </div>
-            { user &&
-            <div className='flex p-2'>
-                <button onClick={handleSignOut} className='text-white font-bold'>Sign out</button>
-            </div>
-}
+            {user &&
+                <div className='flex p-2'>
+                    <button onClick={handleSignOut} className='text-white font-bold'>Sign out</button>
+                </div>
+            }
         </div>
 
     )
